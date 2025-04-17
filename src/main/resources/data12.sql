63122d004dba9b30d322ef80fa3a447bc5792f22
Akash Belide <akashbelide8202@gmail.com>
2025-04-17 18:07:17 -0400

    Added APIs for managing and searching products.

---
+++ src/main/resources/data12.sql
@@ -0,0 +1,7 @@
+INSERT INTO product(name,desc,brand,price,category,release_date,available,quantity)
+VALUES
+    ('Tata Tiago','A Hatchback','TATA Motors',900000,'Cars','2015-01-15',true,400),
+    ('Tata Nexon','A Mid Size SUV','TATA Motors',1500000,'Cars','2017-01-15',true,40),
+    ('Tata Curvv','A Mid Size SUV','TATA Motors',2500000,'Car','2023-01-15',true, 100),
+    ('Tata Harrier', 'A D-Segment SUV','TATA Motors',3000000,'Cars','2019-01-15',true,50),
+    ('Tata Safari','A D-Segment SUV 7 Seater','TATA Motors',4000000,'Car','2019-01-15',true, 60);
